363f4aadc483e0b29b809df061811306bc81538c
Tami Bar <fire17@gmail.com>
2021-01-12 22:51:10 +0200

    heroku debug

--- app.py
+++ app.py
@@ -742,7 +742,7 @@ if __name__ == '__main__':
 	# app.run(debug=True, host='0.0.0.0',use_reloader=False)
 else:
 	flaskRun(master)
-	app.run(debug=True, host='0.0.0.0',use_reloader=False)
+	# app.run(debug=True, host='0.0.0.0',use_reloader=False)
 	print("STARTING APP22222222222")
 	# print("STARTING APP22222222222")
 	# print("STARTING APP22222222222")
